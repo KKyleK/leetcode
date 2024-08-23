89bfecc8d8e915b688636c12b2810234ad8d8fd8
Kyle Koivuneva <kkoivuneva@gmail.com>
2024-08-23 12:38:55 -0600

    Solve leetcode 417

--- graphs.py
+++ graphs.py
@@ -173,22 +173,142 @@ vertex4.neighbors=[vertex2,vertex3]
 
 """
 
-newGraph = cloneGraph(vertex1)
-printGraph(vertex1)
-print()
-printGraph(newGraph)
-print("mutating the original graph:")
-vertex1.val = 10
-vertex2.val=20
-vertex3.val=30
-vertex4.val=40
-#This is working as expected. Only the first graph is mutated, the second remains untouched.
-printGraph(vertex1)
-print()
-printGraph(newGraph)
+# newGraph = cloneGraph(vertex1)
+# printGraph(vertex1)
+# print()
+# printGraph(newGraph)
+# print("mutating the original graph:")
+# vertex1.val = 10
+# vertex2.val=20
+# vertex3.val=30
+# vertex4.val=40
+# #This is working as expected. Only the first graph is mutated, the second remains untouched.
+# printGraph(vertex1)
+# print()
+# printGraph(newGraph)
 
 
-print()
-print()
-print(createAdjacencyList(vertex1))
-print(createAdjacencyList(newGraph))
+# print()
+# print()
+# print(createAdjacencyList(vertex1))
+# print(createAdjacencyList(newGraph))
+
+#994: Rotting oranges
+#Find the minimum number of minutes that must elapse until no cell has a fresh orange.
+def orangesRotting(grid):
+
+    rows = len(grid)
+    columns = len(grid[0])
+    minutes = 0
+    orangesToAvoid = []
+    noChange = False
+    while not noChange:
+        for r in range(rows):
+            for c in range(columns):
+                #Found a rotten orange, change adjacent oranges.
+                if (grid[r][c] == 2) and ((r,c) not in orangesToAvoid):
+
+                    if r+1 < rows:
+                        r = int(r)
+                        c = int(c)
+                        if grid[r+1][c] == 1:
+                            grid[r+1][c] = 2
+                            orangesToAvoid.append((r+1,c))
+                    if r-1 >= 0:
+                        if grid[r-1][c] == 1:
+                            grid[r-1][c] = 2
+                            orangesToAvoid.append((r-1,c))
+                    if c+1 < columns:
+                        if grid[r][c+1] == 1:
+                            grid[r][c+1] = 2
+                            orangesToAvoid.append((r,c+1))
+                    if c-1 >= 0:
+                        if grid[r][c-1] == 1:
+                            grid[r][c-1] = 2
+                            orangesToAvoid.append((r,c-1))
+        if len(orangesToAvoid) == 0:
+            noChange = True
+        else:
+            minutes+=1
+        orangesToAvoid = []
+
+    #Check if all oranges are rotten.
+    for r in range(rows):
+        for c in range(columns):
+            if grid[r][c] == 1:
+                return -1
+    return minutes
+
+# grid = [[2,1,1],[1,1,0],[0,1,1]]
+# print(orangesRotting(grid))
+
+#417: Pacific Atlantic ocean flow
+#Return a list of coordinates representing areas on an island where water can flow into both the pacific ocean and Atlantic ocean.
+
+#Idea: for each coordinate, attempt to move in all directions (up, down, left, right) if moving gets to atlantic/pacific ocean, mark it.
+#If moving gets to both atlantic and pacific, done.
+
+def pacificAtlantic(grid):
+    rows = len(grid)
+    columns = len(grid[0])
+
+    traveled=[]
+    toTravel=[]
+    visitedPacific=False
+    visitedAtlantic=False
+    success=[]
+
+    for r in range (rows):
+        for c in range(columns):
+            toTravel.append((r,c))
+            while (len(toTravel) != 0) and ((visitedPacific != True) or (visitedAtlantic != True)):
+                row, col = toTravel.pop()
+                traveled.append((row, col))
+
+                if (onPacific(row,col)):
+                    visitedPacific = True
+                if (onAtlantic(rows,columns,row,col)):
+                    visitedAtlantic = True
+
+                if row+1 < rows:
+                    if (grid[row+1][col] <= grid[row][col]) and ((row+1,col) not in traveled):
+                        toTravel.append((row+1,col))
+                if row-1 >= 0:
+                    if (grid[row-1][col] <= grid[row][col]) and ((row-1,col) not in traveled):
+                        toTravel.append((row-1,col))
+                if col+1 < columns:
+                    if (grid[row][col+1] <= grid[row][col]) and ((row,col+1) not in traveled):
+                        toTravel.append((row,col+1))
+                if col-1 >= 0:
+                    if (grid[row][col-1] <= grid[row][col]) and ((row,col-1) not in traveled):
+                        toTravel.append((row,col-1))
+
+            if visitedAtlantic == True and visitedPacific == True:
+                success.append([r,c])
+            traveled = []
+            toTravel = []
+            visitedAtlantic = False
+            visitedPacific = False
+
+    return success
+#pacific is far left and top of grid.
+def onPacific(row,col):
+    if row == 0 or col ==0:
+        return True
+    return False
+
+#atlantic is far right and bottom of grid.
+def onAtlantic(rows, columns,row,col):
+    if (row == rows-1) or (col == columns-1):
+        return True
+    return False
+
+
+
+
+heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+print(pacificAtlantic(heights))
+
+heights = [[1,1],[1,1],[1,1]]
+
+print(pacificAtlantic(heights))
