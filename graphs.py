c2956d62eab1fa0b5caafadac436d0ac58ec00ec
Kyle Koivuneva <kkoivuneva@gmail.com>
2024-08-23 18:38:21 -0600

    Attempt leetcode 207

--- graphs.py
+++ graphs.py
@@ -306,9 +306,100 @@ def onAtlantic(rows, columns,row,col):
 
 
 
-heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
-print(pacificAtlantic(heights))
+# heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+# print(pacificAtlantic(heights))
 
-heights = [[1,1],[1,1],[1,1]]
+# heights = [[1,1],[1,1],[1,1]]
 
-print(pacificAtlantic(heights))
+# print(pacificAtlantic(heights))
+
+#207: Course Schedule
+#Num courses represents an array of courses numCourses long.
+#Prerequisites is an array of pairs, where each pair represents the course, followed by the courses prerequisite.
+#Example: numCourses = 2, prerequisites = [[1,0]] means that there are two courses, and to take course 1, you need course 0.
+#Each course can only have 1 prerequisite.
+#
+#Idea: We can build a directed graph to represent the courses and their dependencies.
+#If the graph contains a cycle, then return false, otherwise return true.
+#This is because a cycle means that a two or more courses depend on each other, meaning
+#they can never be completed.
+#
+#Unfortunately my solution below is too inefficient and doesn't pass the runtime requirements for leetcode. Will have to find a better way to find if the graph has no cycles.
+def canFinish(numCourses, prerequisites):
+    #Create graph of courses
+    vertices=[]
+    for c in range(numCourses):
+        vertices.append(Node(c))
+    #Add connections
+    for p in range(len(prerequisites)):
+        vertices[prerequisites[p][0]].neighbors.append(vertices[prerequisites[p][1]])
+
+    #Find if there are any cycles in the directed graph:
+    #For each vertex, go to its neighbor, maintaining a list of nodes traveled.
+    #If we ever repeat a node traveled, there is a cycle.
+    #If a node has two neighbors, maintain a separate list of nodes traveled.
+    toVisit=[] #Looks like: [Node,#] where # is the corresponding array in visited.
+    visited=[]
+
+    for vertex in range(len(vertices)):
+        toVisit.append((vertices[vertex],0))
+        visited.append([]) #We will have at least 1 path.
+
+        while len(toVisit) != 0:
+            currentNode, visitedIndex = toVisit.pop()
+            if currentNode.val in visited[visitedIndex]:
+                return False
+            visited[visitedIndex].append(currentNode.val)
+
+            #Add neighbors. If a neighbor can't be added, delete the whole
+            #chain. Since we are using a stack to determine which path to check next, just pop visited.
+            if len(currentNode.neighbors) == 0:
+                visited.pop()
+            else:
+                for n in range(len(currentNode.neighbors)):
+                    toVisit.append((currentNode.neighbors[n], n+visitedIndex))
+                    #Create a new path, copy what we have seen already in visited.
+                    if n > 0:
+                        copyPath=[]
+                        for i in range(len(visited[visitedIndex])):
+                            copyPath.append(visited[visitedIndex][i])
+                        visited.append(copyPath)
+        toVisit=[]
+        visited=[]
+    return True
+
+##TRUE
+numCourses = 2
+prerequisites = [[1,0]]
+print(canFinish(numCourses, prerequisites))
+
+#FALSE
+numCourses = 2
+prerequisites = [[1,0],[0,1]]
+print(canFinish(numCourses, prerequisites))
+
+#FALSE
+prerequisites = [[2,0],[1,0],[3,1],[3,2],[1,3]]
+numCourses = 4
+print(canFinish(numCourses, prerequisites))
+
+#TRUE
+numCourses = 5
+prerequisites = [[1,4],[2,4],[3,1],[3,2]]
+print(canFinish(numCourses, prerequisites))
+
+#TRUE
+prerequisites =[[1,0],[2,6],[1,7],[6,4],[7,0],[0,5]]
+numCourses=8
+print(canFinish(numCourses, prerequisites))
+
+#TRUE
+prerequisites = [[1,0],[0,3],[0,2],[3,2],[2,5],[4,5],[5,6],[2,4]]
+numCourses=7
+print(canFinish(numCourses, prerequisites))
+
+#Unfortunately my solution takes too long on this, although it does produce "" as the answer.
+numCourses=100
+prerequisites = [[1,0],[2,0],[2,1],[3,1],[3,2],[4,2],[4,3],[5,3],[5,4],[6,4],[6,5],[7,5],[7,6],[8,6],[8,7],[9,7],[9,8],[10,8],[10,9],[11,9],[11,10],[12,10],[12,11],[13,11],[13,12],[14,12],[14,13],[15,13],[15,14],[16,14],[16,15],[17,15],[17,16],[18,16],[18,17],[19,17],[19,18],[20,18],[20,19],[21,19],[21,20],[22,20],[22,21],[23,21],[23,22],[24,22],[24,23],[25,23],[25,24],[26,24],[26,25],[27,25],[27,26],[28,26],[28,27],[29,27],[29,28],[30,28],[30,29],[31,29],[31,30],[32,30],[32,31],[33,31],[33,32],[34,32],[34,33],[35,33],[35,34],[36,34],[36,35],[37,35],[37,36],[38,36],[38,37],[39,37],[39,38],[40,38],[40,39],[41,39],[41,40],[42,40],[42,41],[43,41],[43,42],[44,42],[44,43],[45,43],[45,44],[46,44],[46,45],[47,45],[47,46],[48,46],[48,47],[49,47],[49,48],[50,48],[50,49],[51,49],[51,50],[52,50],[52,51],[53,51],[53,52],[54,52],[54,53],[55,53],[55,54],[56,54],[56,55],[57,55],[57,56],[58,56],[58,57],[59,57],[59,58],[60,58],[60,59],[61,59],[61,60],[62,60],[62,61],[63,61],[63,62],[64,62],[64,63],[65,63],[65,64],[66,64],[66,65],[67,65],[67,66],[68,66],[68,67],[69,67],[69,68],[70,68],[70,69],[71,69],[71,70],[72,70],[72,71],[73,71],[73,72],[74,72],[74,73],[75,73],[75,74],[76,74],[76,75],[77,75],[77,76],[78,76],[78,77],[79,77],[79,78],[80,78],[80,79],[81,79],[81,80],[82,80],[82,81],[83,81],[83,82],[84,82],[84,83],[85,83],[85,84],[86,84],[86,85],[87,85],[87,86],[88,86],[88,87],[89,87],[89,88],[90,88],[90,89],[91,89],[91,90],[92,90],[92,91],[93,91],[93,92],[94,92],[94,93],[95,93],[95,94],[96,94],[96,95],[97,95],[97,96],[98,96],[98,97],[99,97]]
+
+print(canFinish(numCourses, prerequisites))
