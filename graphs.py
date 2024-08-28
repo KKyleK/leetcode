fda33315bb642277fb36b8090e44b88ad8ec092c
Kyle Koivuneva <kkoivuneva7@gmail.com>
2024-08-27 21:37:35 -0600

    Solve leetcode 684

--- graphs.py
+++ graphs.py
@@ -437,38 +437,114 @@ def findInDegreeZeros(numCourses, prerequisites, toVisit, nodesAffected):
                 toVisit.append(i)
     return
 
-##TRUE
-numCourses = 2
-prerequisites = [[1,0]]
-print(canFinish(numCourses, prerequisites))
+# ##TRUE
+# numCourses = 2
+# prerequisites = [[1,0]]
+# print(canFinish(numCourses, prerequisites))
 
-#FALSE
-numCourses = 2
-prerequisites = [[1,0],[0,1]]
-print(canFinish(numCourses, prerequisites))
+# #FALSE
+# numCourses = 2
+# prerequisites = [[1,0],[0,1]]
+# print(canFinish(numCourses, prerequisites))
 
-#FALSE
-prerequisites = [[2,0],[1,0],[3,1],[3,2],[1,3]]
-numCourses = 4
-print(canFinish(numCourses, prerequisites))
+# #FALSE
+# prerequisites = [[2,0],[1,0],[3,1],[3,2],[1,3]]
+# numCourses = 4
+# print(canFinish(numCourses, prerequisites))
 
-#TRUE
-numCourses = 5
-prerequisites = [[1,4],[2,4],[3,1],[3,2]]
-print(canFinish(numCourses, prerequisites))
+# #TRUE
+# numCourses = 5
+# prerequisites = [[1,4],[2,4],[3,1],[3,2]]
+# print(canFinish(numCourses, prerequisites))
 
-#TRUE
-prerequisites =[[1,0],[2,6],[1,7],[6,4],[7,0],[0,5]]
-numCourses=8
-print(canFinish(numCourses, prerequisites))
+# #TRUE
+# prerequisites =[[1,0],[2,6],[1,7],[6,4],[7,0],[0,5]]
+# numCourses=8
+# print(canFinish(numCourses, prerequisites))
 
-#TRUE
-prerequisites = [[1,0],[0,3],[0,2],[3,2],[2,5],[4,5],[5,6],[2,4]]
-numCourses=7
-print(canFinish(numCourses, prerequisites))
+# #TRUE
+# prerequisites = [[1,0],[0,3],[0,2],[3,2],[2,5],[4,5],[5,6],[2,4]]
+# numCourses=7
+# print(canFinish(numCourses, prerequisites))
 
-#TRUE
-numCourses=100
-prerequisites = [[1,0],[2,0],[2,1],[3,1],[3,2],[4,2],[4,3],[5,3],[5,4],[6,4],[6,5],[7,5],[7,6],[8,6],[8,7],[9,7],[9,8],[10,8],[10,9],[11,9],[11,10],[12,10],[12,11],[13,11],[13,12],[14,12],[14,13],[15,13],[15,14],[16,14],[16,15],[17,15],[17,16],[18,16],[18,17],[19,17],[19,18],[20,18],[20,19],[21,19],[21,20],[22,20],[22,21],[23,21],[23,22],[24,22],[24,23],[25,23],[25,24],[26,24],[26,25],[27,25],[27,26],[28,26],[28,27],[29,27],[29,28],[30,28],[30,29],[31,29],[31,30],[32,30],[32,31],[33,31],[33,32],[34,32],[34,33],[35,33],[35,34],[36,34],[36,35],[37,35],[37,36],[38,36],[38,37],[39,37],[39,38],[40,38],[40,39],[41,39],[41,40],[42,40],[42,41],[43,41],[43,42],[44,42],[44,43],[45,43],[45,44],[46,44],[46,45],[47,45],[47,46],[48,46],[48,47],[49,47],[49,48],[50,48],[50,49],[51,49],[51,50],[52,50],[52,51],[53,51],[53,52],[54,52],[54,53],[55,53],[55,54],[56,54],[56,55],[57,55],[57,56],[58,56],[58,57],[59,57],[59,58],[60,58],[60,59],[61,59],[61,60],[62,60],[62,61],[63,61],[63,62],[64,62],[64,63],[65,63],[65,64],[66,64],[66,65],[67,65],[67,66],[68,66],[68,67],[69,67],[69,68],[70,68],[70,69],[71,69],[71,70],[72,70],[72,71],[73,71],[73,72],[74,72],[74,73],[75,73],[75,74],[76,74],[76,75],[77,75],[77,76],[78,76],[78,77],[79,77],[79,78],[80,78],[80,79],[81,79],[81,80],[82,80],[82,81],[83,81],[83,82],[84,82],[84,83],[85,83],[85,84],[86,84],[86,85],[87,85],[87,86],[88,86],[88,87],[89,87],[89,88],[90,88],[90,89],[91,89],[91,90],[92,90],[92,91],[93,91],[93,92],[94,92],[94,93],[95,93],[95,94],[96,94],[96,95],[97,95],[97,96],[98,96],[98,97],[99,97]]
+# #TRUE
+# numCourses=100
+# prerequisites = [[1,0],[2,0],[2,1],[3,1],[3,2],[4,2],[4,3],[5,3],[5,4],[6,4],[6,5],[7,5],[7,6],[8,6],[8,7],[9,7],[9,8],[10,8],[10,9],[11,9],[11,10],[12,10],[12,11],[13,11],[13,12],[14,12],[14,13],[15,13],[15,14],[16,14],[16,15],[17,15],[17,16],[18,16],[18,17],[19,17],[19,18],[20,18],[20,19],[21,19],[21,20],[22,20],[22,21],[23,21],[23,22],[24,22],[24,23],[25,23],[25,24],[26,24],[26,25],[27,25],[27,26],[28,26],[28,27],[29,27],[29,28],[30,28],[30,29],[31,29],[31,30],[32,30],[32,31],[33,31],[33,32],[34,32],[34,33],[35,33],[35,34],[36,34],[36,35],[37,35],[37,36],[38,36],[38,37],[39,37],[39,38],[40,38],[40,39],[41,39],[41,40],[42,40],[42,41],[43,41],[43,42],[44,42],[44,43],[45,43],[45,44],[46,44],[46,45],[47,45],[47,46],[48,46],[48,47],[49,47],[49,48],[50,48],[50,49],[51,49],[51,50],[52,50],[52,51],[53,51],[53,52],[54,52],[54,53],[55,53],[55,54],[56,54],[56,55],[57,55],[57,56],[58,56],[58,57],[59,57],[59,58],[60,58],[60,59],[61,59],[61,60],[62,60],[62,61],[63,61],[63,62],[64,62],[64,63],[65,63],[65,64],[66,64],[66,65],[67,65],[67,66],[68,66],[68,67],[69,67],[69,68],[70,68],[70,69],[71,69],[71,70],[72,70],[72,71],[73,71],[73,72],[74,72],[74,73],[75,73],[75,74],[76,74],[76,75],[77,75],[77,76],[78,76],[78,77],[79,77],[79,78],[80,78],[80,79],[81,79],[81,80],[82,80],[82,81],[83,81],[83,82],[84,82],[84,83],[85,83],[85,84],[86,84],[86,85],[87,85],[87,86],[88,86],[88,87],[89,87],[89,88],[90,88],[90,89],[91,89],[91,90],[92,90],[92,91],[93,91],[93,92],[94,92],[94,93],[95,93],[95,94],[96,94],[96,95],[97,95],[97,96],[98,96],[98,97],[99,97]]
 
-print(canFinish(numCourses, prerequisites))
+# print(canFinish(numCourses, prerequisites))
+
+#684: Redundant connection
+#Find the redundant edge in a graph.
+#Idea: Since an extra edge was added there exists a cycle in the graph. Find the cycle, then return the last edge in the
+#Input that is contained in the cycle.
+def findRedundantConnection(edges):
+    #Use Khan's algorithm to find the cycle. Since it's an undirected graph, we visit, and remove the edges of
+    #nodes with in degree 1 instead of 0.
+    visit = [False] * len(edges) #There are nodes+1 number of edges since one edge was added.
+    toVisit=[]
+    nodesAffected=[]
+    findInDegreeOneUndirected(len(edges), edges, toVisit, nodesAffected)
+
+    while len(toVisit) != 0:
+        while len(toVisit) != 0:
+            #Remove edges for all nodes of in degree = 0:
+            currentNode = toVisit.pop(0)
+            visit[currentNode-1] = True
+            counter = 0
+            while counter < len(edges):
+                if(edges[counter][1]) == currentNode:
+                    nodesAffected.append(edges[counter][0])
+                    edges.pop(counter)
+                    counter-=1
+                if(edges[counter][0]) == currentNode:
+                    nodesAffected.append(edges[counter][1])
+                    edges.pop(counter)
+                    counter-=1
+                counter+=1
+        #After we have visited all nodes with 0 in degree, find more to visit.
+        findInDegreeOneUndirected(len(edges), edges, toVisit, nodesAffected)
+        nodesAffected=[]
+    #Return the cycle:
+    #The cycle will be the LAST edge that includes any of the nodes in the cycle. AKA, if 1,2,3,4 are nodes in the cycle, than if [1,4] is the last edge in edges, that's the answer.
+    cycleNodes=[]
+    for i in range(len(visit)):
+        if not visit[i]:
+            cycleNodes.append(i+1)
+    counter = len(edges)-1
+    while counter >=0:
+        if edges[counter][0] in cycleNodes and edges[counter][1] in cycleNodes:
+            return edges[counter]
+        counter -=1
+
+#Finds nodes with an in degree of 1 in a non directed graph.
+def findInDegreeOneUndirected(numNodes, edges, toVisit, nodesAffected):
+
+    if len(nodesAffected) != 0:
+        for i in nodesAffected:
+            inDegree = 0
+            for j in range(len(edges)):
+                if edges[j][0] == i or edges[j][1] == i:
+                    inDegree +=1
+                    if (inDegree==2):
+                        break
+            if (inDegree < 2):
+                toVisit.append(i)
+
+    else:
+        for i in range(1, numNodes+1):
+            inDegree = 0
+            for j in range(len(edges)):
+                if edges[j][0] == i or edges[j][1] == i:
+                    inDegree +=1
+                    if (inDegree==2):
+                        break
+            if (inDegree < 2):
+                toVisit.append(i)
+    return
+
+
+edges = [[1,2],[1,3],[2,3]]
+print(findRedundantConnection(edges))
+
+edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+print(findRedundantConnection(edges))
